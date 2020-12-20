8587e8a5da6fe9e2de030100cb9a96ffdce4cad5
ARMcK <andrewryanmckinney@gmail.com>
2020-12-20 05:59:36 +0000

    updated sleep for timer

--- core/utils/next_time.py
+++ core/utils/next_time.py
@@ -1,8 +1,9 @@
-from time import time
+from time import time, sleep
 
 def next_time(wait_sec):
     while True:
         now = int(time())
+        sleep(1)
         if now % wait_sec == 0:
             break
 
